2800b88a826de1932976381100cc1bb9642eb625
nibin <nibinbiju222@gmail.com>
2023-11-09 11:39:11 +0530

    bottom navi added

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -6,6 +6,11 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Container(
+        child: Center(
+          child: Text('home'),
+        ),
+      ),
+    );
   }
 }
