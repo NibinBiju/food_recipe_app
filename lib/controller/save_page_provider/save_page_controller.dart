8286404391c5929a7df4e15319a3ffda514fa987
Nibin C B <140245310+NibinBiju@users.noreply.github.com>
2023-11-14 23:22:12 +0530

    created detail page (#64)

--- lib/controller/save_page_provider/save_page_controller.dart
+++ lib/controller/save_page_provider/save_page_controller.dart
@@ -3,11 +3,24 @@ import 'package:recipe_app/model/save_page_model/save_page_cookbook_model.dart';
 
 class SavePageProvider with ChangeNotifier {
   final List<CreateCookBookModel> mySavedRecipes = [
-    CreateCookBookModel(index: 0, cookBookName: 'Favorite'),
+    CreateCookBookModel(index: 0, cookBookName: 'Favorite', recipes: [
+      SavedRecipes(
+          image: 'assets\images\Rectangle 17 (1).png',
+          rating: '4.3',
+          recipeName: 'Chicken',
+          time: '30m')
+    ]),
   ];
+
+  final List<SavedRecipes> savedRecipes = [];
 
   void createNewCookBook(CreateCookBookModel createCookBookModel) {
     mySavedRecipes.add(createCookBookModel);
+    notifyListeners();
+  }
+
+  void addNewRecipe(SavedRecipes recipes) {
+    savedRecipes.add(recipes);
     notifyListeners();
   }
 }
